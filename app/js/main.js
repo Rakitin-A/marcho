2cb2cc4d64d190ef9045c9b37702fb7c9db94fb6
Rakitin-A <rakitin.alexey@bk.ru>
2021-10-21 16:53:17 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,49 @@
 $(function () {
+
+    
+    $('.shop-content__filter-btn').on('click', function () {
+        $('.shop-content__filter-btn').removeClass('.shop-content__filter-btn--active');
+        $(this).addClass('.shop-content__filter-btn--active');
+    });
+    
+    
+    $('.button-list').on('click', function () {
+        $('.product-item').addClass('.product-item--list');
+    });
+    
+    $('.button-grid').on('click', function () {
+        $('.product-item').removeClass('.product-item--list');
+    });
+    
+    
+    
+    $('.select-style').styler();
+
+    // * фильтр shop page *
+
+    $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+            //  обозначение диапозона цен без передвижения ползунка
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+        onChange: function (data) {
+            //  передача данных фильтра в span
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+    });
+
+
+
+
+
+
+
+
+
     $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
@@ -11,16 +56,16 @@ $(function () {
         starWidth: "17px",
         normalFill: "#ccccce",
         ratedFill: "#ffc35b",
-        readOnly: true
+        readOnly: true,
     });
 
 
 
     // ***************************
     // * дата завершения таймера *
-    // ***************************
+
     const endtime = 'December 31 2021, 23:59 GMT+0300';
-    
+
 
     // приводим к стандартному виду 03:04:05, вместо 3:4:5
     function makeCorrectDate(uncorrectDate) {
@@ -214,8 +259,10 @@ $(function () {
             }
             document.querySelector('.uncorrect__seconds').textContent = correct__seconds;
         }
+
     }
     setTime('timer', endtime);
+
 
 
 });
