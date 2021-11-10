72bbaea672750ceb64314ebae473024c49bb6f86
Rakitin-A <rakitin.alexey@bk.ru>
2021-11-10 17:04:40 +0300

    added product slider & filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,43 @@
 $(function () {
 
-    
+    // * слайдер product *
+    $('.product-slide__thumb').slick({
+        asNavFor: '.product-slide__big',
+        focusOnSelect: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false,
+
+    });
+    $('.product-slide__big').slick({
+        asNavFor: '.product-slide__thumb',
+        draggable: false,
+        arrows: false,
+        fade:true
+    });
+
+
+
+
+
     $('.shop-content__filter-btn').on('click', function () {
         $('.shop-content__filter-btn').removeClass('.shop-content__filter-btn--active');
         $(this).addClass('.shop-content__filter-btn--active');
     });
-    
-    
+
+
     $('.button-list').on('click', function () {
         $('.product-item').addClass('.product-item--list');
     });
-    
+
     $('.button-grid').on('click', function () {
         $('.product-item').removeClass('.product-item--list');
     });
-    
-    
-    
-    $('.select-style').styler();
+
+
+
+    $('.select-style, .product-one__item-num').styler();
 
     // * фильтр shop page *
 
@@ -35,6 +55,7 @@ $(function () {
             $('.filter-price__to').text(data.to);
         },
     });
+
 
 
 
